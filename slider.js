14defd2a80295e7b31f26e72d216934069a01f96
OksanaFedotova <72733674+OksanaFedotova@users.noreply.github.com>
2022-12-16 19:41:35 +0300

    update main

--- slider.js
+++ slider.js
@@ -13,9 +13,23 @@ const arrDiv = ['1','2','3'].map((el, i) => {
   div.style.backgroundColor = color;
   div.style.height = '500px';
   div.style.transition = 'transform .5s';
-  div.style.transform = `translate3d(-${i * 100}%, 0, 0)`;
   container.append(div);
 });
-body.append(container);
-const arrowLeft = document.createElement('button');
-//https://habr.com/ru/post/501258/
+body.append(container)
+const buttonPrev = document.createElement('button');
+buttonPrev.innerText = 'Prev';
+const buttonNext = document.createElement('button');
+buttonNext.innerText = 'Next';
+let counter = 0;
+buttonNext.addEventListener('click', () => {
+  counter++;
+  if (counter > 1) counter = 2;
+  container.style.transform = `translate3d(-${counter * 100}%, 0, 0)`;
+});
+buttonPrev.addEventListener('click', () => {
+  if (counter <= 1) counter = 1;
+  const x = counter*100-100;
+  container.style.transform = `translate3d(-${x}%, 0, 0)`;
+  counter--;
+})
+body.append(buttonPrev, buttonNext);
